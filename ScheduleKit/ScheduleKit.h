c804059e4e318f6ad604cf6537fe81a8f5fe20c0
Guillem Servera <gservera@aurumcode.com>
2014-12-24 14:14:30 +0100

    Initial Commit

---
+++ ScheduleKit/ScheduleKit.h
@@ -0,0 +1,19 @@
+//
+//  ScheduleKit.h
+//  ScheduleKit
+//
+//  Created by Guillem on 24/12/14.
+//  Copyright (c) 2014 Guillem Servera. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ScheduleKit.
+FOUNDATION_EXPORT double ScheduleKitVersionNumber;
+
+//! Project version string for ScheduleKit.
+FOUNDATION_EXPORT const unsigned char ScheduleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScheduleKit/PublicHeader.h>
+
+
