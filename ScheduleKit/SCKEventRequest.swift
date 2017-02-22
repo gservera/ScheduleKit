164dabdbf0e02e5a7204dacece3b3e350c61fb10
Guillem Servera <guillem@gservera.com>
2017-02-22 12:37:18 +0100

    Update license and copyright notices

--- ScheduleKit/SCKEventRequest.swift
+++ ScheduleKit/SCKEventRequest.swift
@@ -3,7 +3,7 @@
  *  ScheduleKit
  *
  *  Created:    Guillem Servera on 16/07/2015.
- *  Copyright:  © 2014-2016 Guillem Servera (https://github.com/gservera)
+ *  Copyright:  © 2014-2017 Guillem Servera (https://github.com/gservera)
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
--- ScheduleKit/Views/SCKWeekView.swift
+++ ScheduleKit/Views/SCKWeekView.swift
@@ -3,7 +3,7 @@
  *  ScheduleKit
  *
  *  Created:    Guillem Servera on 29/10/2016.
- *  Copyright:  © 2016 Guillem Servera (https://github.com/gservera)
+ *  Copyright:  © 2016-2017 Guillem Servera (https://github.com/gservera)
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
